c1bd1d53e431691edfb062719344e30ea61deeef
Jabyrd3sk <Jabyrd3sk@gmail.com>
2025-09-04 18:15:31 +0100

    Applying changes for part 4b

--- Part3_ServerProgrammingWithNodeJSsExpressLibrary/backend/utils/config.js
+++ Part3_ServerProgrammingWithNodeJSsExpressLibrary/backend/utils/config.js
@@ -1,6 +1,24 @@
+// Part 4a, Structure of backend application, introduction to testing
+// require('dotenv').config()
+
+// const PORT = process.env.PORT
+// const MONGODB_URI = process.env.MONGODB_URI
+
+// module.exports = { MONGODB_URI, PORT }
+
+
+
+
+
+// Part 4b Testing backend
+
+// Test environment
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
+
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI
 
 module.exports = { MONGODB_URI, PORT }
