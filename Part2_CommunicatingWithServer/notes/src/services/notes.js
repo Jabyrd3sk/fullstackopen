13a98b229c78f9586c7c02420be4a58b16014ff0
Jabyrd3sk <Jabyrd3sk@gmail.com>
2025-09-22 23:57:44 +0100

    Applying changes to Part 5a

--- Part2_CommunicatingWithServer/notes/src/services/notes.js
+++ Part2_CommunicatingWithServer/notes/src/services/notes.js
@@ -1,20 +1,57 @@
+// import axios from 'axios'
+// // const baseUrl = 'http://localhost:3001/api/notes'
+// const baseUrl = '/api/notes'
+
+// const getAll = () => {
+//     const request = axios.get(baseUrl)
+//     const nonExisting = {
+//         id: 10000,
+//         content: 'This note is not saved to server',
+//         important: true
+//     }
+//     return request.then(response => response.data.concat(nonExisting))
+// }
+
+// const create = newObject => {
+//     const request = axios.post(baseUrl, newObject)
+//     return request.then(response => response.data)
+// }
+
+// const update = (id, newObject) => {
+//     const request =  axios.put(`${baseUrl}/${id}`, newObject)
+//     return request.then(response => response.data)
+// }
+
+
+// export default { getAll, create, update }
+
+
+
+
+
+// Part 5a: Login in frontend
 import axios from 'axios'
 // const baseUrl = 'http://localhost:3001/api/notes'
 const baseUrl = '/api/notes'
 
-const getAll = () => {
-    const request = axios.get(baseUrl)
-    const nonExisting = {
-        id: 10000,
-        content: 'This note is not saved to server',
-        important: true
-    }
-    return request.then(response => response.data.concat(nonExisting))
+let token = null
+
+const setToken = newToken => {
+    token = `Bearer ${newToken}`
 }
 
-const create = newObject => {
-    const request = axios.post(baseUrl, newObject)
+const getAll = () => {
+    const request = axios.get(baseUrl)
     return request.then(response => response.data)
+}
+
+const create = async newObject => {
+    const config = {
+        headers: { Authorization: token }
+    }
+
+    const response = await axios.post(baseUrl, newObject, config)
+    return response.data
 }
 
 const update = (id, newObject) => {
@@ -23,4 +60,4 @@ const update = (id, newObject) => {
 }
 
 
-export default { getAll, create, update }
+export default { getAll, create, update, setToken }
