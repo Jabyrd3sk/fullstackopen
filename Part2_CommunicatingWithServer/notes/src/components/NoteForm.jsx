bbe4aa78cf1ab8b72ce1e9ea2f1e53d0c336b55f
Jabyrd3sk <Jabyrd3sk@gmail.com>
2025-10-01 15:29:31 +0100

    Applying changes to Part 5c

--- Part2_CommunicatingWithServer/notes/src/components/NoteForm.jsx
+++ Part2_CommunicatingWithServer/notes/src/components/NoteForm.jsx
@@ -18,31 +18,70 @@
 // Part 5b: Props children and proptypes
 
 // state of the forms
-import { useState } from "react"
+// import { useState } from "react"
+
+// const NoteForm = ({ createNote }) => {
+//     const [newNote, setNewNote] = useState('')
+
+//     const addNote = (event) => {
+//         event.preventDefault()
+//         createNote({
+//             content: newNote,
+//             important: true
+//         })
+
+//         setNewNote('')
+//     }
+
+//     return (
+//         <div>
+//             <h2>Create a new note</h2>
+
+//             <form onSubmit={addNote}>
+//                 <input value={newNote} onChange={event => setNewNote(event.target.value)} />
+//                 <button type="submit">save</button>
+//             </form>
+//         </div>
+//     )
+// }
+
+// export default NoteForm
+
+
+
+
+
+// Part 5c: Testing react apps
+// About finding the elements
+import { useState } from 'react'
 
 const NoteForm = ({ createNote }) => {
-    const [newNote, setNewNote] = useState('')
+  const [newNote, setNewNote] = useState('')
 
-    const addNote = (event) => {
-        event.preventDefault()
-        createNote({
-            content: newNote,
-            important: true
-        })
+  const addNote = event => {
+    event.preventDefault()
+    createNote({
+      content: newNote,
+      important: true
+    })
 
-        setNewNote('')
-    }
+    setNewNote('')
+  }
 
-    return (
-        <div>
-            <h2>Create a new note</h2>
+  return (
+    <div>
+      <h2>Create a new note</h2>
 
-            <form onSubmit={addNote}>
-                <input value={newNote} onChange={event => setNewNote(event.target.value)} />
-                <button type="submit">save</button>
-            </form>
-        </div>
-    )
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={event => setNewNote(event.target.value)}
+          placeholder="write note content here"
+        />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  )
 }
 
 export default NoteForm
