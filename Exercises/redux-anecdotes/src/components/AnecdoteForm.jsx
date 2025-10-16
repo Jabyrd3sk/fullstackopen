24dc9c7910b16d39729f31e33b768e8b1862df7a
Jabyrd3sk <Jabyrd3sk@gmail.com>
2025-10-16 17:36:14 +0100

    Applying chnages to redux-anecdotes (Exercise 6.10-6.13)

--- Exercises/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ Exercises/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,22 +1,24 @@
-import { useDispatch } from "react-redux"
+import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
+import { showNotification } from '../reducers/notificationReducer'
 
 const AnecdoteForm = () => {
-    const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
-    const addAnecdote = (event) => {
-        event.preventDefault()
-        const content = event.target.anecdote.value
-        event.target.anecdote.value = ''
-        dispatch(createAnecdote(content))
-    }
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    dispatch(createAnecdote({ content, votes: 0, id: Math.random().toString() }))
+    dispatch(showNotification(`You added '${content}'`, 5))
+  }
 
-    return (
-        <form onSubmit={addAnecdote}>
-            <input name="anecdote" />
-            <button type="submit">add</button>
-        </form>
-    )
+  return (
+    <form onSubmit={addAnecdote}>
+      <input name="anecdote" />
+      <button type="submit">create</button>
+    </form>
+  )
 }
 
 export default AnecdoteForm
--- Exercises/redux-anecdotes/src/components/AnecdoteList.jsx
+++ Exercises/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,30 +1,34 @@
-import { useDispatch, useSelector } from "react-redux"
-import { voteAnecdote } from "../reducers/anecdoteReducer"
+import { useSelector, useDispatch } from 'react-redux'
+import { voteAnecdote } from '../reducers/anecdoteReducer'
+import { showNotification } from '../reducers/notificationReducer'
 
 const AnecdoteList = () => {
-    const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
-    const anecdotes = useSelector(({ anecdotes, filter }) => {
-    const filtered = anecdotes.filter(a =>
-      a.content.toLowerCase().includes(filter.toLowerCase())
-    )
-    return [...filtered].sort((a, b) => b.votes - a.votes)
+  const anecdotes = useSelector(({ anecdotes, filter }) => {
+    return anecdotes
+      .filter(a => a.content.toLowerCase().includes(filter.toLowerCase()))
+      .sort((a, b) => b.votes - a.votes)
   })
-    
 
-    return (
-        <div>
-            {anecdotes.map(anecdote =>
-                <div key={anecdote.id}>
-                    <div>{anecdote.content}</div>
-                    <div>
-                        has {anecdote.votes}
-                        <button onClick={() => dispatch(voteAnecdote(anecdote.id))}>vote</button>
-                    </div>
-                </div>
-            )}
+  const handleVote = (anecdote) => {
+    dispatch(voteAnecdote(anecdote.id))
+    dispatch(showNotification(`You voted for '${anecdote.content}'`, 5))
+  }
+
+  return (
+    <div>
+      {anecdotes.map(anecdote => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
         </div>
-    )
+      ))}
+    </div>
+  )
 }
 
 export default AnecdoteList
