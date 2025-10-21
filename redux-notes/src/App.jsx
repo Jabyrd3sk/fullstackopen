64cfdcdf6f912d0d10706d5a2417166f4b36bdfe
Jabyrd3sk <Jabyrd3sk@gmail.com>
2025-10-21 10:12:42 +0100

    Applying changes to part 6c

--- redux-notes/src/App.jsx
+++ redux-notes/src/App.jsx
@@ -1,16 +1,54 @@
-import NoteForm from "./components/NoteForm";
+// import NoteForm from "./components/NoteForm";
+// import Notes from './components/Notes'
+// import VibilityFilter from "./components/VisibilityFilter"
+
+// const App = () => {
+//   const filterSelected = (value) => {
+//     console.log(value)
+//   }
+
+//   return (
+//     <div>
+//       <NoteForm />
+//       <VibilityFilter />
+//       <Notes />
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+
+
+
+
+
+
+// Part 6c: Communicating with server in a redux application
+// Initializing the store with data fetched from server
+
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
+import NoteForm from './components/NoteForm'
 import Notes from './components/Notes'
-import VibilityFilter from "./components/VisibilityFilter"
+import VisibilityFilter from './components/VisibilityFilter'
+import { setNotes } from './reducers/noteReducer'
+import noteService from './services/notes'
 
 const App = () => {
-  const filterSelected = (value) => {
-    console.log(value)
-  }
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    noteService.getAll().then((notes) => dispatch(setNotes(notes)))
+  }, [dispatch])
 
   return (
     <div>
       <NoteForm />
-      <VibilityFilter />
+      <VisibilityFilter />
       <Notes />
     </div>
   )
--- redux-notes/src/reducers/noteReducer.js
+++ redux-notes/src/reducers/noteReducer.js
@@ -66,51 +66,117 @@
 // Part 6b: Many Reducers
 // Redux Toolkit and Refactoring the Store Configuration
 
-import { createSlice, current } from '@reduxjs/toolkit'
-const initialState = [
-    {
-        content: 'reducer defines how redux store works',
-        important: true,
-        id: 1,
-    },
-    {
-        content: 'state of store can contain any data',
-        important: false,
-        id: 2
-    },
-]
+// import { createSlice, current } from '@reduxjs/toolkit'
+// const initialState = [
+//     {
+//         content: 'reducer defines how redux store works',
+//         important: true,
+//         id: 1,
+//     },
+//     {
+//         content: 'state of store can contain any data',
+//         important: false,
+//         id: 2
+//     },
+// ]
 
-const generateId = () => Number((Math.random() * 1000000).toFixed(0))
+// const generateId = () => Number((Math.random() * 1000000).toFixed(0))
+
+// const noteSlice = createSlice({
+//     name: 'notes',
+//     initialState,
+//     reducers: {
+//         createNote(state, action) {
+//             const content = action.payload
+//             state.push({
+//                 content,
+//                 important: false,
+//                 id: generateId(),
+//             })
+//         },
+        
+//         toggleImportanceOf(state, action) {
+//             const id = action.payload
+//             const noteToChange = state.find(n => n.id === id)
+//             const changedNote =  {
+//                 ...noteToChange,
+//                 important: !noteToChange.important
+//             }
+
+//             console.log(current(state))
+
+//             return state.map(note =>
+//                 note.id !== id ? note: changedNote
+//             )
+//         },
+//     },
+// })
+
+// export const { createNote, toggleImportanceOf } = noteSlice.actions
+// export default noteSlice.reducer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Part 6c: Communicating with server in a redux application
+// Initializing the store with data fetched from server
+
+import { createSlice } from '@reduxjs/toolkit'
+import noteService from '../services/notes'
+import notes from '../services/notes'
 
 const noteSlice = createSlice({
-    name: 'notes',
-    initialState,
-    reducers: {
-        createNote(state, action) {
-            const content = action.payload
-            state.push({
-                content,
-                important: false,
-                id: generateId(),
-            })
-        },
-        
-        toggleImportanceOf(state, action) {
-            const id = action.payload
-            const noteToChange = state.find(n => n.id === id)
-            const changedNote =  {
-                ...noteToChange,
-                important: !noteToChange.important
-            }
-
-            console.log(current(state))
-
-            return state.map(note =>
-                note.id !== id ? note: changedNote
-            )
-        },
+  name: 'notes',
+  initialState: [],
+  reducers: {
+    createNote(state, action) {
+      state.push(action.payload)
     },
+    toggleImportanceOf(state, action) {
+      const id = action.payload
+      const noteToChange = state.find((n) => n.id === id)
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important,
+      }
+      return state.map((note) => (note.id !== id ? note : changedNote))
+    },
+    setNotes(state, action) {
+      return action.payload
+    },
+  },
 })
 
-export const { createNote, toggleImportanceOf } = noteSlice.actions
+const { createNote, setNotes } = noteSlice.actions
+
+export const initializeNotes = () => {
+  return async (dispatch) => {
+    const notes = await noteService.getAll()
+    dispatch(setNotes(notes))
+  }
+}
+
+export const appendNote = (content) => {
+  return async (dispatch) => {
+    const newNote = await noteService.createNew(content)
+    dispatch(noteSlice.actions.createNote(newNote))
+  }
+}
+
+export const { toggleImportanceOf } = noteSlice.actions
+
 export default noteSlice.reducer
