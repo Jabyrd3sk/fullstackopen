5735113b1c239a886f00a25c8a297c1abd6539e2
Jabyrd3sk <Jabyrd3sk@gmail.com>
2025-10-30 17:11:36 +0100

    Applying changes for part 7b & 7c

--- router-app/src/router-app-v1.jsx
+++ router-app/src/router-app-v1.jsx
@@ -1,5 +1,219 @@
+// React bootstrap
+// import ReactDOM from 'react-dom/client'
+// import { useState } from 'react'
+// import { Table, Form, Button, Alert, Navbar, Nav } from 'react-bootstrap'
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Link,
+//   Navigate,
+//   useParams,
+//   useNavigate,
+//   useMatch
+// } from "react-router-dom"
+
+
+// const Home = () => (
+//   <div>
+//     <h2>TKTL notes app</h2>
+//     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+//   </div>
+// )
+
+// const Note = ({ note }) => {
+
+//   return (
+//     <div>
+//       <h2>{note.content}</h2>
+//       <div>{note.user}</div>
+//       <div><strong>{note.important ? 'important' : ''}</strong></div>
+//     </div>
+//   )
+// }
+
+// const Notes = ({ notes }) => (
+//   <div>
+//     <h2>Notes</h2>
+//     <Table striped>
+//       <tbody>
+//         {notes.map(note =>
+//           <tr key={note.id}>
+//             <td>
+//               <Link to={`/notes/${note.id}`}>
+//                 {note.content}
+//               </Link>
+//             </td>
+//             <td>
+//               {note.user}
+//             </td>
+//           </tr>
+//         )}
+//       </tbody>
+//     </Table>      
+//   </div>
+// )
+
+// const Users = () => (
+//   <div>
+//     <h2>TKTL notes app</h2>
+//     <ul>
+//       <li>Jabir Sani</li>
+//       <li>Matti Luukkainen</li>
+//       <li>Hassan Muhd</li>
+//     </ul>
+//   </div>
+// )
+
+// const Login = (props) => {
+//   const navigate = useNavigate()
+
+//   const onSubmit = (event) => {
+//     event.preventDefault()
+//     props.onLogin('Jabir')
+//     navigate('/')
+//   }
+
+//   return (
+//     <div>
+//       <h2>login</h2>
+//       <Form onSubmit={onSubmit}>
+//         <Form.Group>
+//           <Form.Label>username:</Form.Label>
+//           <Form.Control
+//             type='text'
+//             name='username'
+//           />
+//         </Form.Group>
+//         <Form.Group>
+//           <Form.Label>password</Form.Label> 
+//           <Form.Control
+//             type='password'
+//           />
+//         </Form.Group>
+//         <Button variant='primary' type='submit'>
+//           login
+//         </Button>
+//       </Form>
+      
+//     </div>
+//   )
+// }
+
+// const App = () => {
+//   const [notes, setNotes] = useState([
+//     {
+//       id: 1,
+//       content: 'HTML is easy',
+//       important: true,
+//       user: 'Jabir Sani'
+//     },
+//     {
+//       id: 2,
+//       content: 'Browser can execute only JavaScript',
+//       important: false,
+//       user: 'Matti Luukkainen'
+//     },
+//     {
+//       id: 3,
+//       content: 'Most important methods of HTTP-protocol are GET and POST',
+//       important: true,
+//       user: 'Hassan Muhd'
+//     }
+//   ])
+
+//   const match = useMatch('/notes/:id')
+//   const note = match
+//     ? notes.find(note => note.id === Number(match.params.id))
+//     : null
+
+//   const [user, setUser] = useState(null)
+//   const [message, setMessage] = useState(null)
+
+//   const login = (user) => {
+//     setUser(user)
+//     setMessage(`welcome ${user}`)
+//     setTimeout(() => {
+//       setMessage(null)
+//     }, 1000)
+//   }
+//   const padding = {
+//     padding: 5
+//   }
+
+//   return (
+//     <div className='container'>
+//       {( message && <Alert variant='success'>{message}</Alert> )}
+
+//       <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+//         <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+//         <Navbar.Collapse id='responsive-navbar-nav'>
+//           <Nav className='me-auto'>
+//             <Nav.Link href='#' as='span'>
+//               <Link style={padding} to="/">home</Link>
+//             </Nav.Link>
+//             <Nav.Link href='#' as='span'>
+//               <Link style={padding} to="/notes">notes</Link>
+//             </Nav.Link>
+//             <Nav.Link href='#' as='span'>
+//               <Link style={padding} to="/users">users</Link>
+//             </Nav.Link>
+//             <Nav.Link href='#' as='span'>
+//               {user 
+//                 ? <em style={padding}>{user} logged in</em>
+//                 : <Link style={padding} to="/login">login</Link>
+//               }
+//             </Nav.Link>
+//           </Nav>
+//         </Navbar.Collapse>        
+//       </Navbar>
+
+//       <Routes>
+//         <Route path="/notes/:id" element={<Note note={note} />} />
+//         <Route path="/notes" element={<Notes notes={notes} />} />
+//         <Route path="/users" element={user ? <Users /> : <Navigate replace to="/login" />} />
+//         <Route path="/login" element={<Login onLogin={login} />} />
+//         <Route path="/" element={<Home />} />
+//       </Routes>
+
+//       <div>
+//         <br />
+//         <em>Note app, Department of Computer Science 2025</em>
+//       </div>
+//     </div>
+//   )
+// }
+
+// ReactDOM.createRoot(document.getElementById('root')).render(<Router><App /></Router>)
+
+
+
+
+
+
+
+
+
+
+// Material UI
 import ReactDOM from 'react-dom/client'
 import { useState } from 'react'
+
+import {
+  Container,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Paper,
+  TextField,
+  Button,
+  AppBar,
+  Toolbar,
+  Alert,
+  IconButton
+} from '@mui/material'
 
 import {
   BrowserRouter as Router,
@@ -34,13 +248,22 @@ const Note = ({ note }) => {
 const Notes = ({ notes }) => (
   <div>
     <h2>Notes</h2>
-    <ul>
-      {notes.map(note =>
-        <li key={note.id}>
-          <Link to={`/notes/${note.id}`}>{note.content}</Link>
-        </li>
-      )}
-    </ul>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableBody>
+          {notes.map(note => (
+            <TableRow key={note.id}>
+              <TableCell>
+                <Link to={`/notes/${note.id}`}>{note.content}</Link>
+              </TableCell>
+              <TableCell>
+                {note.user}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>      
   </div>
 )
 
@@ -69,12 +292,16 @@ const Login = (props) => {
       <h2>login</h2>
       <form onSubmit={onSubmit}>
         <div>
-          username: <input />
+          <TextField label="username" />
         </div>
         <div>
-          password: <input type='password' />
+          <TextField label="password" type='password' />
         </div>
-        <button type="submit">login</button>
+        <div>
+          <Button variant='contained' color='primary' type='submit'>
+            login
+          </Button>
+        </div>
       </form>
     </div>
   )
@@ -102,34 +329,50 @@ const App = () => {
     }
   ])
 
-  const [user, setUser] = useState(null)
-
   const match = useMatch('/notes/:id')
-
   const note = match
     ? notes.find(note => note.id === Number(match.params.id))
     : null
 
+  const [user, setUser] = useState(null)
+  const [message, setMessage] = useState(null)
 
   const login = (user) => {
     setUser(user)
+    setMessage(`welcome ${user}`)
+    setTimeout(() => {
+      setMessage(null)
+    }, 1000)
   }
-
   const padding = {
     padding: 5
   }
 
   return (
-    <div>
-      <div>
-        <Link style={padding} to="/">home</Link>
-        <Link style={padding} to="/notes">notes</Link>
-        <Link style={padding} to="/users">users</Link>
-        {user
-          ? <em>{user} logged in</em>
-          : <Link style={padding} to="/login">login</Link>
-        }
-      </div>
+    <Container>
+    <div className='container'>
+      {( message && <Alert severity='success'>{message}</Alert> )}
+
+      <AppBar position='static'>
+        <Toolbar>
+          <Button color='inherit' component={Link} to="/">
+            home
+          </Button>
+          <Button color='inherit' component={Link} to="/notes">
+            notes
+          </Button>
+          <Button color='inherit' component={Link} to="/users">
+            users
+          </Button>
+          {user
+            ? <em>{user} logged in</em>
+            : <Button color='inherit' component={Link} to="/login">
+              login
+              </Button>
+            }
+        </Toolbar>
+      </AppBar>
+
       <Routes>
         <Route path="/notes/:id" element={<Note note={note} />} />
         <Route path="/notes" element={<Notes notes={notes} />} />
@@ -137,11 +380,13 @@ const App = () => {
         <Route path="/login" element={<Login onLogin={login} />} />
         <Route path="/" element={<Home />} />
       </Routes>
+
       <div>
         <br />
         <em>Note app, Department of Computer Science 2025</em>
       </div>
     </div>
+    </Container>
   )
 }
 
